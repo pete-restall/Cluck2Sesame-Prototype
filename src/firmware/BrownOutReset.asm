f6328a9c3ab8a70431d6aba9fcc919054288caa9
pete-restall <pete@restall.net>
2016-10-02 13:11:15 +0100

    Removed the breadboarding ISR code.

--- src/firmware/BrownOutReset.asm
+++ src/firmware/BrownOutReset.asm
@@ -1,17 +1,14 @@
 	#include "p16f685.inc"
-	#include "TailCalls.inc"
 	radix decimal
 
 BrownOutReset code
 	global initialiseAfterBrownOutReset
 
 initialiseAfterBrownOutReset:
-	extern isrInitialise
-
 	banksel PCON
 	bsf PCON, NOT_POR
 	bsf PCON, NOT_BOR
 
-	tcall isrInitialise
+	return
 
 	end
