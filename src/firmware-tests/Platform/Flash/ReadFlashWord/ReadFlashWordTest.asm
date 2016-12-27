c296626cec4f3412a52edb1d63efd65b43eb43fa
pete-restall <pete@restall.net>
2016-12-27 12:44:09 +0000

    Added readFlashWord() which will be required for the CORDIC tables.

---
+++ src/firmware-tests/Platform/Flash/ReadFlashWord/ReadFlashWordTest.asm
@@ -0,0 +1,27 @@
+	#include "Mcu.inc"
+	#include "FarCalls.inc"
+	#include "Flash.inc"
+	#include "TestFixture.inc"
+
+	radix decimal
+
+ReadFlashWordTest code
+	global testArrange
+
+testArrange:
+
+testAct:
+	loadFlashAddressOf magicWord
+	fcall readFlashWord
+
+testAssert:
+	.aliasForAssert EEDAT, _a
+	.assert "_a == 0x79, 'Expected EEDAT == 0x79.'"
+
+	.aliasForAssert EEDATH, _a
+	.assert "_a == 0x35, 'Expected EEDATH == 0x35.'"
+	return
+
+magicWord dw 0x3579
+
+	end
--- src/firmware/Platform/Flash.asm
+++ src/firmware/Platform/Flash.asm
@@ -4,18 +4,22 @@
 
 Flash code
 	global readFlashWordAsPairOfSevenBitBytes
+	global readFlashWord
 
 readFlashWordAsPairOfSevenBitBytes:
+	call readFlashWord
+	banksel EEDAT
+	rlf EEDAT, W
+	rlf EEDATH, W
+	bcf EEDAT, 7
+	return
+
+readFlashWord:
 	banksel EECON1
 	bsf EECON1, EEPGD
 	bsf EECON1, RD
 	nop
 	nop
-
-	banksel EEDAT
-	rlf EEDAT, W
-	rlf EEDATH, W
-	bcf EEDAT, 7
 	return
 
 	end
