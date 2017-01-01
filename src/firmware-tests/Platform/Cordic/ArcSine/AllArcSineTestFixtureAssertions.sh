106f90328694ee43c6de630a4a3453f2db02f886
pete-restall <pete@restall.net>
2017-01-01 15:34:16 +0000

    Added an exhaustive test for all CORDIC arcSine() calls.

---
+++ src/firmware-tests/Platform/Cordic/ArcSine/AllArcSineTestFixtureAssertions.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+FILE_PREFIX="AllArcSineTestFixture";
+
+objcopy -I ihex ${FILE_PREFIX}.0000-3fff.log -O binary ${FILE_PREFIX}.0000-3fff.bin;
+objcopy -I ihex ${FILE_PREFIX}.4000-7fff.log -O binary ${FILE_PREFIX}.4000-7fff.bin;
+objcopy -I ihex ${FILE_PREFIX}.8000-bfff.log -O binary ${FILE_PREFIX}.8000-bfff.bin;
+objcopy -I ihex ${FILE_PREFIX}.c000-ffff.log -O binary ${FILE_PREFIX}.c000-ffff.bin;
+
+cat ${FILE_PREFIX}.{0000-3fff,4000-7fff,8000-bfff,c000-ffff}.bin > ${FILE_PREFIX}.bin;
+
+diff -b ${FILE_PREFIX}.bin ${CORDIC_DIR}/cordic-fixed-arcsine-table.bin;
+if [ $? -eq 0 ]; then
+	echo "[PASSED] All arcsine values match verified values";
+else
+	echo "[FAILED] Differences found in calculated versus verified arcsine values";
+fi;
