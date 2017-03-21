2e043accb7cce33e5e220de848a17d133e0d5b94
pete-restall <pete@restall.net>
2017-03-21 17:08:46 +0000

    Sunrise / Sunset algorithm refinement and testing.  The algorithm looks to be complete now (across UK latitudes / longitudes), although some refactoring is required.

--- src/firmware/SunriseSunset/InterpolationStates.asm
+++ src/firmware/SunriseSunset/InterpolationStates.asm
@@ -53,16 +53,8 @@ storePositiveAccumulatorFromA:
 		goto nextState
 
 storeNegativeAccumulatorFromA:
+		fcall negateA32
 		call storeAccumulatorFromA
-		banksel accumulatorUpperHigh
-		movlw 0xff
-		movwf accumulatorUpperHigh
-		movwf accumulatorUpperLow
-		comf accumulatorLowerHigh
-		comf accumulatorLowerLow
-		incf accumulatorLowerLow
-		btfsc STATUS, Z
-		incf accumulatorLowerHigh
 
 nextState:
 		banksel sunriseSunsetNextState
@@ -207,6 +199,73 @@ storeAccumulatorFromAShiftedRight8Bits:
 	defineSunriseSunsetStateInSameSection SUN_STATE_INTERPOLATE_LATITUDE3
 		call loadAccumulatorIntoA
 		call loadLookupReferenceMinuteIntoB
+		fcall add32
+		call storeAccumulatorFromA
+
+		setSunriseSunsetState SUN_STATE_INTERPOLATE_LONGITUDE
+		returnFromSunriseSunsetState
+
+
+	defineSunriseSunsetStateInSameSection SUN_STATE_INTERPOLATE_LONGITUDE
+loadLongitudeOffsetMultipliedByFourIntoDividend:
+		bcf STATUS, C
+		banksel longitudeOffset
+		rlf longitudeOffset, W
+		banksel RAD
+		movwf RAD
+		btfss STATUS, C
+		goto positiveLongitudeOffset
+
+negativeLongitudeOffsetIntoPositive:
+		comf RAD
+		incf RAD
+		bcf STATUS, C
+
+positiveLongitudeOffset:
+		rlf RAD
+		clrf RAA
+		clrf RAB
+		clrf RAC
+
+loadTenIntoDivisor:
+		clrf RBC
+		movlw 10
+		movwf RBD
+
+divideAndStoreQuotientAndRemainder:
+		fcall div32x16
+		call storeLookupIndexFromA
+
+		setSunriseSunsetState SUN_STATE_INTERPOLATE_LONGITUDE2
+		returnFromSunriseSunsetState
+
+
+	defineSunriseSunsetStateInSameSection SUN_STATE_INTERPOLATE_LONGITUDE2
+roundUpIfRemainderIsAtLeastFive:
+		banksel lookupIndexRemainderLow
+		movf lookupIndexRemainderLow, W
+		sublw 4
+		movf lookupIndexLow, W
+		btfss STATUS, C
+		incf lookupIndexLow, W
+
+loadOffsetNumberOfMinutesIntoB:
+		banksel RBA
+		clrf RBA
+		clrf RBB
+		clrf RBC
+		movwf RBD
+
+loadNonOffsetNumberOfMinutesIntoA:
+		call loadAccumulatorIntoA
+		banksel longitudeOffset
+
+offsetMinutesAreNegativeIfLongitudeOffsetIsPositive:
+		btfsc longitudeOffset, 7
+		goto adjustNumberOfMinutesInAccumulatorForLongitudeOffset
+		fcall negateB32
+
+adjustNumberOfMinutesInAccumulatorForLongitudeOffset:
 		fcall add32
 		call storeAccumulatorFromA
 
