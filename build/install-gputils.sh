b594d338e74edc8847817d9451ab5d09ef06ba56
pete-restall <pete@restall.net>
2017-04-15 20:01:45 +0100

    Removed the string table optimisation patch from the gputils build as it's now been added into the gputils trunk.

--- build/install-gputils.sh
+++ build/install-gputils.sh
@@ -2,7 +2,6 @@
 mkdir gputils-trunk
 cd gputils-trunk
 svn checkout svn://svn.code.sf.net/p/gputils/code/trunk .
-patch gputils/libgputils/gpwriteobj.c < ../gputils-patch/gputils/libgputils/gpwriteobj.c.patch
 cd gputils
 ./configure --prefix=/usr
 make
