55b0902f66bcb3f0d6dda15da68fb2a204f0549b
pete-restall <pete@restall.net>
2017-04-28 09:01:36 +0100

    Build fix - temporarily removed the gpsim oscillator patch.

--- build/install-gpsim.sh
+++ build/install-gpsim.sh
@@ -8,7 +8,7 @@ autoheader
 automake --force-missing --add-missing
 patch src/Makefile.in < ../gpsim-patch/src/Makefile.in.patch
 patch src/p16f88x.cc < ../gpsim-patch/src/p16f88x.cc.patch
-patch src/14bit-registers.cc < ../gpsim-patch/src/14bit-registers.cc.patch
+# patch src/14bit-registers.cc < ../gpsim-patch/src/14bit-registers.cc.patch
 autoconf
 ./configure --disable-gui --prefix=/usr
 make
