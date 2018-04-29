99119b6748847a7b6aeadc4bee42cbed726f7fdc
pete-restall <pete@restall.net>
2018-04-29 08:40:19 +0100

    Using known-good version of gputils trunk.

--- build/install-gputils.sh
+++ build/install-gputils.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 mkdir gputils-trunk
 cd gputils-trunk
-svn checkout svn://svn.code.sf.net/p/gputils/code/trunk .
+svn checkout -r1311 svn://svn.code.sf.net/p/gputils/code/trunk .
 cd gputils
 ./configure --prefix=/usr
 make
